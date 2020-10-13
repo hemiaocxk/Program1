4f366191a48f146c9a90980e2274ba5c7131f40b
hemiaocxk <72697491+hemiaocxk@users.noreply.github.com>
2020-10-13 14:17:11 +0800

    Add files via upload

--- CPU.java
+++ CPU.java
@@ -4,6 +4,11 @@ public class CPU {
 	private int speed;
 	  String brand;	 
 	  public int getspeed(){
+		  if(speed>0){
+			  speed=speed;
+			}else{
+				System.out.println("Error");
+			}
 		return speed;
 		
 	}
@@ -13,6 +18,7 @@ public class CPU {
 	  }
 	 public void setspeed(int speed){
 		this.speed=speed;
+		
 	}
 	 public void setbrand(String brand){
 		 this.brand=brand;
